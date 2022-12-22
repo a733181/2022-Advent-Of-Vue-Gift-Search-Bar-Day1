e29450ee2b56c91de08853302d43948b67720576
Syuan <a733181@gmail.com>
2022-12-22 08:31:30 +0800

    新增 api search

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
-    <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
+    <input
+      type="text"
+      class="p-2 border-2 border-gray-dark"
+      v-model="searchTerm"
+      placeholder="Start typing..."
+      @change="searchGift"
+    />
     <ul class="list-disc">
-      <li>Display suggestions here</li>
+      <li v-for="(prod, index) in searchData" :key="index">{{ prod.title }}</li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 
 const searchTerm = ref('')
+const searchData = ref([])
 
-const findProducts = async term => {}
+const findProducts = async term => {
+  const res = await fetch(`https://dummyjson.com/products/search?q=${term}`)
+  const data = await res.json()
+  if (term) {
+    searchData.value = data.products
+  } else {
+    searchData.value = []
+  }
+}
 
-watch(searchTerm, newTerm => findProducts(newTerm))
+const searchGift = () => {
+  findProducts(searchTerm.value)
+}
 </script>
